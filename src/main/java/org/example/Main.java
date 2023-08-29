43fbfb3c94aa1444d38c118ea90b7cbaf2f5b84b
JoergWolff <neuerfisch@wolffclan.de>
2023-08-29 17:20:57 +0200

    remove import Java.Array

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,6 @@ package org.example;
 import org.example.Book.Book;
 import org.example.Library.Library;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
 
